f692b48333e937a9df05adc3b6d77b6bc475a150
Arya Rakha <33478892+arrakh@users.noreply.github.com>
2024-07-06 17:08:50 +0700

    init commit

---
+++ ViewPrefabDatabase.cs
@@ -0,0 +1,11 @@
+﻿using Arr.ScriptableDatabases;
+using UnityEngine;
+
+namespace Arr.ViewModuleSystem
+{
+    [CreateAssetMenu(menuName = "View Prefab Database")]
+    public class ViewPrefabDatabase : ObjectScriptableDatabase<View>
+    {
+        
+    }
+}
